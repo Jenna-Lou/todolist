22feaf09aea7196c05d2137f278eb042d563ed5b
Jenna Lou <jennalouronquillo@yahoo.com>
2016-04-22 08:47:26 -0700

    changed the position

--- public_html/js/blog.js
+++ public_html/js/blog.js
@@ -30,3 +30,14 @@ function Posts (args) {
     this.content = args.content || "";
     this.authorEmail = args.authorEmail || "";
 }
+
+ $('.dropdown-button').dropdown({
+      inDuration: 200,
+      outDuration: 125,
+      constrain_width: false, // Does not change width of dropdown to that of the activator
+      hover: true, // Activate on hover
+      gutter: 0, // Spacing from edge
+      belowOrigin: false, // Displays dropdown below the button
+      alignment: 'left' // Displays dropdown with edge aligned to the left of button
+    }
+  );
